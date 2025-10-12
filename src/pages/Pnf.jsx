e973f82cd0d1da52e5b952bfa20f2adf1d7a7d2f
AdithyaBijimon <adithyabijimon5@gmail.com>
2025-10-12 14:11:15 +0530

    Auth page updated

--- src/pages/Pnf.jsx
+++ src/pages/Pnf.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Pnf = () => {
   return (
-    <div>Pnf</div>
+    
+
+    <div className='w-full h-screen flex items-center justify-center flex-col'>
+
+      <img src="https://media4.giphy.com/avatars/404academy/kGwR3uDrUKPI.gif" alt="" width={'500px'}/>
+
+      <Link to={'/'}><button className='bg-yellow-500 px-3 py-2  text-black font-bold  mt-5 cursor-pointer'>Back to Home</button></Link>
+    </div>
+    
+    
   )
 }
 
